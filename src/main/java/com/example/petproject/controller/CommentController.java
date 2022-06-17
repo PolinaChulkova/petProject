edecd391bef4bf4b7e44a1e199855997ee7c6345
PolinaChulkova <shelkova1937@gmail.com>
2022-06-17 16:02:30 +0500

    Изменения контроллеров в CommentController

--- src/main/java/com/example/petproject/controller/CommentController.java
+++ src/main/java/com/example/petproject/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.example.petproject.controller;
 import com.example.petproject.model.Comment;
 import com.example.petproject.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,29 +14,31 @@ public class CommentController {
     private final CommentService commentService;
 
     @RequestMapping(method = RequestMethod.GET, value = "/{commentId}")
-    public Comment getComment(@PathVariable Long commentId) {
-        return commentService.findCommentById(commentId);
+    @ResponseBody
+    public ResponseEntity<Comment> getComment(@PathVariable Long commentId) {
+        return ResponseEntity.ok().body(commentService.findCommentById(commentId));
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/{authorId}/{newsId}")
-    public String createComment(@PathVariable Long authorId,
+    @ResponseBody
+    public ResponseEntity<Comment> createComment(@PathVariable Long authorId,
                                 @PathVariable Long newsId,
                                 @RequestBody Comment comment) {
-        commentService.createComment(authorId, newsId, comment);
-        return "New comment created";
+        return ResponseEntity.ok().body(commentService.createComment(authorId, newsId, comment));
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
-    public String updateComment(@PathVariable Long id,
-                                @RequestBody Comment comment) {
-        Comment oldComment = commentService.findCommentById(id);
-        commentService.updateComment(comment, oldComment);
-        return "Comment update";
+    @ResponseBody
+    public ResponseEntity<Comment> updateComment(@PathVariable Long id,
+                                @RequestBody Comment editedComment) {
+        Comment comment = commentService.findCommentById(id);
+        commentService.updateComment(editedComment, comment);
+        return ResponseEntity.ok().body(comment);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-    public String deleteComment(@PathVariable Long id) {
-        commentService.deleteById(id);
-        return "Comment deleted";
+    @ResponseBody
+    public ResponseEntity<Comment> deleteComment(@PathVariable Long id) {
+        return ResponseEntity.ok().body(commentService.deleteById(id));
     }
 }
